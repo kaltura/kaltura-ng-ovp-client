8076c9649cdb2634d40834600ab50adb58bfa7f9
Eran Sakal <eran@sakalim.com>
2017-11-28 08:46:44 +0200

    fix: use ngc during tranpiling to support angular-cli based applications

--- gulpfile.js
+++ gulpfile.js
@@ -7,76 +7,52 @@ var $ = require('gulp-load-plugins')();
 var tslint = require("gulp-tslint");
 var del = require('del');
 var runSequence = require('run-sequence');
-const argv = require("yargs").argv;
 var jeditor = require("gulp-json-editor");
+const ngc = require('gulp-ngc') ;
+
 var merge = require('merge2');  // Require separate installation
-//var KarmaServer = require('karma').Server;
 
 //set configuration
 const tsconfig = require('./tsconfig.json').compilerOptions;
 
+const tempDist = './.tmp';
+const dist = './dist';
+
 // clean the contents of the distribution directory
-gulp.task('clean', function () {
-  return del(['.tmp', 'dist/**/*'], {force: true});
-});
+gulp.task('clean', ['clean:tmp','clean:dist']);
 
 gulp.task('clean:tmp', function () {
-  return del(['.tmp'], {force: true});
+  return del([tempDist], {force: true});
 });
 
 gulp.task('clean:dist', function () {
-  return del(['dist/**/*'], {force: true});
+  return del([`${dist}/**/*`], {force: true});
 });
 
-gulp.task('scripts:library', function () {
-  return compileAppScripts();
+gulp.task('library:scripts', function () {
+  return ngc('tsconfig.json');
 });
 
-function compileAppScripts() {
-  var tsProject = $.typescript.createProject(tsconfig);
-  var opt = {
-    tsProject: tsProject,
-    inPath: ["src/**/*.ts",
-      "!src/tests/**/*.ts",
-      "!src/api/tests/**/*.ts",
-      "!**/*.spec.ts",
-      "!dist/**",
-      "!.tmp/**",
-      "!node_modules/**"],
-    inBase : './src',
-    outPath: './.tmp/dist'
+function createNpmPackageJson(packageContentAsString) {
+
+  if (packageContentAsString) {
+
+    var packageIndent = getJsonIndent(packageContentAsString) || 4;
+    var packageContent = JSON.parse(packageContentAsString);
+
+    packageContent.devDependencies = {};
+    packageContent.peerDependencies = packageContent.dependencies;
+    packageContent.dependencies = {};
+    packageContent.scripts = {};
+    packageContent.private = false;
+
+    if (packageContent.config && packageContent.config.npmDistDirectory) {
+      delete packageContent.config.npmDistDirectory;
+    }
+
+    return JSON.stringify(packageContent,null,packageIndent);
   }
-  return compileTS(opt);
 }
-
-function compileTS(opt) {
-  var tsResult = gulp.src(opt.inPath, {base : opt.inBase})
-  // .pipe(tslint({
-  // 	configuration : 'tslint.json',
-  // 	formatter: "prose"
-  // }))
-  // .pipe(tslint.report({
-  // 	emitError: false,
-  // 	summarizeFailureOutput : true
-  // }))
-    .pipe($.sourcemaps.init()) // sourcemaps will be generated
-    .pipe(opt.tsProject($.typescript.reporter.fullReporter(true)))
-    .on('error', function (error) {
-      //if (argv.production) {
-      //var log = gutil.log, colors = gutil.colors;
-      //log('Typescript compilation exited with ' + colors.red(error));
-      process.exit(1);
-      //}
-    });
-
-  return merge([ // this task is finished when the IO of both operations are done
-    tsResult.dts.pipe(gulp.dest(opt.outPath)),
-    tsResult.js
-      .pipe($.sourcemaps.write()) // sourcemaps are added to the .js file
-      .pipe(gulp.dest(opt.outPath))
-  ]);
-}
-
 gulp.task('extras', function () {
   var packageFileResult = gulp.src(['package.json'], {base: './'})
     .pipe(jeditor(function(json) {
@@ -87,9 +63,9 @@ gulp.task('extras', function () {
       json.scripts = {};
       return json; // must return JSON object.
     }))
-    .pipe(gulp.dest('./.tmp/dist'));
+    .pipe(gulp.dest('./.tmp'));
 
-  var extraResult = gulp.src(['./LICENSE','./.gitignore','./README.md','.npmignore'], {base: './'}).pipe(gulp.dest('./.tmp/dist'));
+  var extraResult = gulp.src(['./LICENSE','./.gitignore','./README.md','.npmignore'], {base: './'}).pipe(gulp.dest('./.tmp'));
 
 
   return merge([
@@ -99,11 +75,12 @@ gulp.task('extras', function () {
 });
 
 gulp.task('copyTmpToDist', ['clean:dist'], function () {
-  return gulp.src(['./.tmp/dist/**/*','./.tmp/dist/**/.*'], {base: './.tmp/dist/'}).pipe(gulp.dest('./dist'));
+  return gulp.src([`./${tempDist}/**/*`, `./${tempDist}/**/.*`], {base: tempDist}).pipe(gulp.dest(dist));
 });
 
 gulp.task('build',function () {
-  return runSequence('clean:tmp','scripts:library','extras','copyTmpToDist','clean:tmp');
+
+  return runSequence('clean:tmp','library:scripts','extras','copyTmpToDist','clean:tmp');
 });
 
 gulp.task('watch', [], function () {
